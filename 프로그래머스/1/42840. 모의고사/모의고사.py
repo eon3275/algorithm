31c4b29db38275a24a3cb3b2899d524ede8107bc
Baek changjoo <ggulddu5@gmail.com>
2026-04-19 17:37:07 +0900

    [level 1] Title: 모의고사, Time: 7.31 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,12 +1,11 @@
 def solution(answers):
     answer = []
-    supo = [[[1,2,3,4,5],5,0],[[2,1,2,3,2,4,2,5], 8,0],[[3,3,1,1,2,2,4,4,5,5], 10, 0]]
+    patterns = [[1,2,3,4,5],[2, 1, 2, 3, 2, 4, 2, 5], [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
+    scores = [0,0,0]
     for i, ans in enumerate(answers):
-        for sp in supo:
-            if sp[0][i%sp[1]]==ans:
-                sp[2]+=1
-    max_correct = max(supo[0][2], supo[1][2], supo[2][2])
-    for i in range(3):
-        if max_correct==supo[i][2]:
-            answer.append(i+1)
-    return answer
+        for idx, pattern in enumerate(patterns):
+            if pattern[i%len(pattern)]==ans:
+                scores[idx]+=1
+    max_score = max(scores)
+    
+    return [i+1 for i, s in enumerate(scores) if max_score==s]
