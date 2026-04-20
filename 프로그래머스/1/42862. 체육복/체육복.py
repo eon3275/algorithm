be61cb7b90f20390276c1844f8ad43e8ecbff3e1
Baek changjoo <ggulddu5@gmail.com>
2026-04-20 17:34:40 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,10 +1,9 @@
 def solution(n, lost, reserve):
     _lost = set(lost)-set(reserve)
     _reserve = set(reserve)-set(lost)
-    for r in _reserve:
+    for r in sorted(_reserve):
         if r-1 in _lost:
             _lost.remove(r-1)
         elif r+1 in _lost:
             _lost.remove(r+1)
-    answer = n - len(_lost)
-    return answer
+    return n-len(_lost)
