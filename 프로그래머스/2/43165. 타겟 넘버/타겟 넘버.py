2318776943a6c42d9c517c08f566a1ebc055ae28
Baek Changjoo <ggulddu5@gmail.com>
2026-07-13 20:23:12 +0900

    Update 타겟 넘버.py

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,15 +1,7 @@
-from collections import deque
 def solution(numbers, target):
     n = len(numbers)
-    answer = 0
-    q = deque()
-    q.append((0, 0))
-    while q:
-        i, curr = q.popleft()
+    def dfs(i, curr):
         if i==n:
-            if curr==target:
-                answer+=1
-            continue
-        q.append((i+1, curr+numbers[i]))
-        q.append((i+1, curr-numbers[i]))
-    return answer
+            return 1 if curr==target else 0
+        return dfs(i+1, curr+numbers[i])+dfs(i+1, curr-numbers[i])
+    return dfs(0,0)
