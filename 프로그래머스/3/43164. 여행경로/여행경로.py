284421b80b7de87045aac51b7a143b96ea929d06
Baek Changjoo <ggulddu5@gmail.com>
2026-07-15 02:06:00 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+def solution(tickets):
+    adj = defaultdict(list)
+    for src, dst in sorted(tickets, reverse=True):
+        adj[src].append(dst)
+    stack = ['ICN']
+    answer = []
+    while stack:
+        curr = stack[-1]
+        if not adj[curr]:
+            answer.append(stack.pop())
+        else:
+            stack.append(adj[curr].pop())
+    return answer[::-1]
