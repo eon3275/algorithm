47c96700b445e0d1f3d448502f431e89fe6d3c1e
Baek Changjoo <ggulddu5@gmail.com>
2026-07-21 22:13:33 +0900

    [Passed] 성적 평가 (216ms, 42MB)

--- hsat/성적 평가/grading.py
+++ hsat/성적 평가/grading.py
@@ -1,25 +1,20 @@
 N = int(input())
-scores = [[(val, i) for i, val in enumerate(map(int, input().split()))] for _ in range(3)]
+scores = [list(map(int, input().split())) for _ in range(3)]
 
 # Please write your code here.
 total_score = [0]*N
 for j in range(N):
     for i in range(3):
-        total_score[j] += scores[i][j][0]
-    total_score[j] = (total_score[j], j)
+        total_score[j]+=scores[i][j]
 scores.append(total_score)
 for i in range(4):
-    scores[i].sort(reverse=True)
-
-ranks = [0]*N
-for i in range(4):
-    rank = 1
-    prev_score = 1001
-    for j in range(N):
-        score, idx = scores[i][j]
-        if score<prev_score:
-            rank = j+1
-        prev_score = score
-        ranks[idx] = rank
-    print(*ranks)
-        
+    max_value = 3000 if i==3 else 1000
+    cnt = [0]*(max_value+1)
+    for score in scores[i]:
+        cnt[score]+=1
+    rank = [0]*(max_value+1)
+    acc = 0
+    for s in range(max_value,-1,-1):
+        rank[s] = acc+1
+        acc+=cnt[s]
+    print(*(rank[s] for s in scores[i]))
