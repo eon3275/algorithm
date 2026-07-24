1a4cb09d0ef1b86bac9494a71d08171e8dffd0a3
Baek Changjoo <ggulddu5@gmail.com>
2026-07-25 00:40:54 +0900

    [Passed] 자동차 테스트 (237ms, 27MB)

---
+++ hsat/자동차 테스트/car-test.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+n, q = map(int, input().split())
+efficiency = list(map(int, input().split()))
+m = [int(input()) for _ in range(q)]
+
+# Please write your code here.
+index = {}
+efficiency.sort()
+for i, e in enumerate(efficiency):
+    index[e] = i
+for m_i in m:
+    if m_i not in index:
+        print(0)
+    else:
+        less = index[m_i]
+        print(less*(n-less-1))
